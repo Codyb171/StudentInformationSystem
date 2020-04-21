79c9224e63fc5c952ad11d29a8dbb51e08cc43bb
codyb171 <codybishop171@gmail.com>
2020-04-20 20:10:47 -0400

    Updated by Cody

--- src/SISApplication.java
+++ src/SISApplication.java
@@ -1,7 +1,64 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SISApplication
 {
+    public static ArrayList<Student> studentArray = new ArrayList<>();
+    public static ArrayList<Course> courseArray = new ArrayList<>();
+    public static ArrayList<Instructor> instructorArray = new ArrayList<>();
     public static void main(String[] args)
-    {
+    {     int inUse = 0;
+          int choice;
+          Scanner input = new Scanner(System.in);
+          while (inUse == 0)
+          {
+              printMenu();
+              choice = input.nextInt();
+              if (choice == 1)
+              {
+                  
+              }
+              if(choice == 2)
+              {
 
+              }
+              if(choice == 3)
+              {
+
+              }
+              if(choice == 4)
+              {
+
+              }
+              if (choice == 5)
+              {
+
+              }
+              if(choice == 6)
+              {
+
+              }
+              if(choice == 7)
+              {
+                  inUse = 1;
+              }
+          }
+    }
+    public static void printMenu()
+    { System.out.println("Student Management System \n " +
+            "Please make a menu choice below:");
+        for(int i = 0; i< 33; i++)
+        {
+            System.out.print("-");
+        }
+        System.out.println("1: Create a Course \n" +
+                "2: Create a Student \n" +
+                "3: Add a Student to a Course \n" +
+                "4: Remove a Student from a Course \n" +
+                "5: Assign an Instructor to a Course \n" +
+                "6: Print a Roster for a Course \n" +
+                "7: Exit System");
+        System.out.print("Choice: ");
     }
 }
+

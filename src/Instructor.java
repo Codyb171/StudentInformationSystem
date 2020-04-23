27ee95eabaaa615913a0c877b7bd16476225048f
brown <brown@LAPTOP-GLDG1MMC>
2020-04-23 19:11:36 -0400

    Updated by Cody

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,8 +1,10 @@
-public class Instructor
-{
+import java.util.Scanner;
+
+public class Instructor {
+    Scanner errorCorrect = new Scanner(System.in);
     private String name;
     private String prefix;
-    private String officeLocation; 
+    private String officeLocation;
     private String department;
     private String email;
     private int instructorID;
@@ -17,18 +19,35 @@ public class Instructor
         setInstructorID(nextInstructID);
         nextInstructID++;
     }
+
     public void setName(String Name) {
         this.name = Name;
     }
+
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
+
     public void setDepartment(String department) {
         this.department = department;
     }
+
     public void setEmail(String email) {
+        int test = 0;
+        while (test == 0) {
+            for (int i = 0; i < email.length(); i++) {
+                if (email.startsWith("@", i)) {
+                    test = 1;
+                }
+            }
+            if (test == 0) {
+                System.out.print("Invalid Email, Missing an '@', Please Re-enter: ");
+                email = errorCorrect.next();
+            }
+        }
         this.email = email;
     }
+
     public void setOfficeLocation(String officeLocation) {
         this.officeLocation = officeLocation;
     }
@@ -47,7 +66,7 @@ public class Instructor
 
     public String toString() {
         String formattedString;
-        formattedString = String.format("InstructorID: %-9s %n Instructor Name: %-2s %-20s %n Department: %-20s %n Office: %-20s %n Email: %-30s",
+        formattedString = String.format("InstructorID: %-9s %nInstructor Name: %-2s %-20s %nDepartment: %-20s %nOffice: %-20s %nEmail: %-30s",
                 this.instructorID, this.prefix, this.name, this.department, this.officeLocation, this.email);
         return formattedString;
     }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,7 @@
-public class Student
-{
+import java.util.Scanner;
+
+public class Student {
+    Scanner errorCorrect = new Scanner(System.in);
     private String firstName;
     private String lastName;
     private String studentYear;
@@ -8,9 +10,8 @@ public class Student
     private String studentEmail;
     private int studentID;
     private static int nextStudentID = 1000;
-    
-    public Student(String name, int year, String major, double GPA, String email)
-    {
+
+    public Student(String name, int year, String major, double GPA, String email) {
         int space = splitName(name);
         setFirstName(name.substring(0,space));
         setLastName(name.substring(space));
@@ -21,11 +22,10 @@ public class Student
         setStudentID(nextStudentID);
         nextStudentID++;
     }
-    public int splitName(String name)
-    {
+
+    public int splitName(String name) {
         int where =0;
-        for(int i =0; i < name.length(); i++)
-        {
+        for(int i =0; i < name.length(); i++) {
             if (name.startsWith(" ", i)) {
                 where = i;
             }
@@ -33,12 +33,12 @@ public class Student
 
         return where;
     }
-    public void setFirstName(String first)
-    {
+
+    public void setFirstName(String first) {
         firstName = first;
     }
-    public void setLastName(String last)
-    {
+
+    public void setLastName(String last) {
         lastName=last;
     }
 
@@ -63,50 +63,49 @@ public class Student
     }
 
     public void setStudentEmail(String email) {
-        for (int i = 0; i < email.length(); i++) {
-            if (email.charAt(i) == '@') {
-                studentEmail = email;
-            } else {
-                break;
+        int test = 0;
+        while (test == 0) {
+            for (int i = 0; i < email.length(); i++) {
+                if (email.startsWith("@", i)) {
+                    test = 1;
+                }
+            }
+            if (test == 0) {
+                System.out.print("Invalid Email, Missing an '@', Please Re-enter: ");
+                email = errorCorrect.next();
             }
         }
+        studentEmail = email;
     }
-    public void setStudentYear(int year)
-    {
-        if(year == 1)
-        {
+
+    public void setStudentYear(int year) {
+        if(year == 1) {
             studentYear = "Freshman";
-        }
-        else if(year == 2)
-        {
+        } else if(year == 2) {
             studentYear = "Sophomore";
-        }
-        else if(year == 3)
-        {
+        } else if(year == 3) {
             studentYear = "Junior";
-        }
-        else if(year == 4)
-        {
+        } else if(year == 4) {
             studentYear = "Senior";
         }
     }
-    public String getStudentYear()
-    {
+
+    public String getStudentYear() {
         return this.studentYear;
     }
-    public String getStudentMajor()
-    {
+
+    public String getStudentMajor() {
         return this.studentMajor;
     }
-    public void setStudentMajor(String major)
-    {
+
+    public void setStudentMajor(String major) {
         studentMajor = major;
     }
-    public String toString()
-    {
+
+    public String toString() {
         String printOut;
-        printOut = String.format("Student ID:%-6d Student Name:%-10s, %-15s Major: %-15s Year: %-15s",
-                this.studentID, this.lastName, this.firstName,this.studentMajor,this.studentYear);
+        printOut = String.format("Student ID:%-6d Student Name: %-10s, %-15s Major: %-10s Year: %-15s",
+                this.studentID, this.lastName, this.firstName, this.studentMajor, this.studentYear);
         return printOut;
     }
 }

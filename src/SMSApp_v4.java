78759796413570cdfece731f8112079f1aefddc0
telph <telph@192.168.0.12>
2020-04-27 11:56:55 -0400

    Merge origin/master - Trying to fix synch error1

--- src/SMSApp_v4.java
+++ src/SMSApp_v4.java
@@ -1,5 +1,11 @@
+// Authors: Cody Bishop, Scott Baker, Tanner Elphee, Harry Brown, and Austin Lindsey
+// Dr. Ezell | CIS 331
+// Purpose: Main Method for the Applicaiton 
+//  Link to JMU OneDrive: https://dukesjmuedu-my.sharepoint.com/:f:/g/personal/elpheeti_dukes_jmu_edu/Er7HS_yJyotJnm-GCDn661sBVS7mJo0PJ3uKvEESinPfwA?e=t1WDck
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.*;
+
 
 public class SMSApp_v4 extends Application {
 

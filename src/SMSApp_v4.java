e1286b76905b526f31c76b16d994292e56a2fa15
telph <telph@192.168.0.12>
2020-04-27 12:14:56 -0400

    Merge origin/master - Trying to fix synch error1

--- src/SMSApp_v4.java
+++ src/SMSApp_v4.java
@@ -2,19 +2,41 @@
 // Dr. Ezell | CIS 331
 // Purpose: Main Method for the Applicaiton 
 //  Link to JMU OneDrive: https://dukesjmuedu-my.sharepoint.com/:f:/g/personal/elpheeti_dukes_jmu_edu/Er7HS_yJyotJnm-GCDn661sBVS7mJo0PJ3uKvEESinPfwA?e=t1WDck
+
+//Use MenuOrder example as reference
+// Assignment of Duties
+//   Build GUI - Tanner and Austin
+//   Develop the new needed methods(most difficult) - Cody
+//   Prepare Database files - William
+//   Develop Dataabase saving feature (Phase 3) - Harry and Cody
+
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.Scene;
+import javafx.event.*;
+
+// For ArrayList
 import java.util.*;
 
+// These enable ComboBoxes
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 
 public class SMSApp_v4 extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
     public void start(Stage primaryStage) {
 
+    }
+    
+    
+    
+    public static void main(String[] args) {
+    launch(args);
     }
 }

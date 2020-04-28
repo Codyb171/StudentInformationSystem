209d5195f34ba25c12a0f0cbc8eb5fc612838de6
telph <telph@192.168.0.12>
2020-04-28 16:21:53 -0400

    Merge origin/master - Trying to fix synch error1

--- src/SMSApp_v4.java
+++ src/SMSApp_v4.java
@@ -29,7 +29,7 @@ import javafx.geometry.Pos;
 
 //These are needed for Oracle SQL database bridge provided by Dr.Ezell
 import java.sql.*;
-//import oracle.jdbc.pool.*; Error shown until Database is connected. 
+import oracle.jdbc.pool.*; //Error shown until Database is connected. 
  
 
 public class SMSApp_v4 extends Application {
@@ -50,52 +50,52 @@ public class SMSApp_v4 extends Application {
 ////   NEED DATABASE LINKED FOR THIS BRIDGE TO WORK. UNCOMMENT TO SEE ERRORS! 
     
     
-//    //DATABASE BRIDGE NETBEANS FREE FROM DR.EZELL - Refer to InventoryUI Example. 
-//    public void sendDBCommand(String sqlQuery)
-//        {
-//            // Set up your connection strings
-//            // IF YOU ARE IN CIS330 NOW: use YOUR Oracle Username/Password
-//            String URL = "jdbc:oracle:thin:@localhost:1521:XE";
-//            String userID = "shenu"; // Change to YOUR Oracle username
-//            String userPASS = "shenu"; // Change to YOUR Oracle password
-//            OracleDataSource ds;
-//
-//            // Clear Box Testing - Print each query to check SQL syntax
-//            //  sent to this method.
-//            // You can comment this line out when your program is finished
-//            System.out.println(sqlQuery);
-//
-//            // Lets try to connect
-//            try
-//            {
-//                // instantiate a new data source object
-//                ds = new OracleDataSource();
-//                // Where is the database located? Web? Local?
-//                ds.setURL(URL);
-//                // Send the user/pass and get an open connection.
-//                dbConn = ds.getConnection(userID,userPASS);
-//                // When we get results
-//                //  -TYPE_SCROLL_SENSITIVE means if the database data changes we
-//                //   will see our resultset update in real time.
-//                //  -CONCUR_READ_ONLY means that we cannot accidentally change the
-//                //   data in our database by using the .update____() methods of
-//                //   the ResultSet class - TableView controls are impacted by
-//                //   this setting as well.
-//                commStmt = dbConn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-//                // We send the query to the DB. A ResultSet object is instantiated
-//                //  and we are returned a reference to it, that we point to from
-//                // dbResults.
-//                // Because we declared dbResults at the datafield level
-//                // we can see the results from anywhere in our Class.
-//                dbResults = commStmt.executeQuery(sqlQuery); // Sends the Query to the DB
-//                // The results are stored in a ResultSet structure object
-//                // pointed to by the reference variable dbResults
-//                // Because we declared this variable globally above, we can use
-//                // the results in any method in the class.
-//            }
-//            catch (SQLException e)
-//            {
-//                System.out.println(e.toString());
-//            }
-//        }
+    //DATABASE BRIDGE NETBEANS FREE FROM DR.EZELL - Refer to InventoryUI Example. 
+    public void sendDBCommand(String sqlQuery)
+        {
+            // Set up your connection strings
+            // IF YOU ARE IN CIS330 NOW: use YOUR Oracle Username/Password
+            String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+            String userID = "shenu"; // Change to YOUR Oracle username
+            String userPASS = "shenu"; // Change to YOUR Oracle password
+            OracleDataSource ds;
+
+            // Clear Box Testing - Print each query to check SQL syntax
+            //  sent to this method.
+            // You can comment this line out when your program is finished
+            System.out.println(sqlQuery);
+
+            // Lets try to connect
+            try
+            {
+                // instantiate a new data source object
+                ds = new OracleDataSource();
+                // Where is the database located? Web? Local?
+                ds.setURL(URL);
+                // Send the user/pass and get an open connection.
+                dbConn = ds.getConnection(userID,userPASS);
+                // When we get results
+                //  -TYPE_SCROLL_SENSITIVE means if the database data changes we
+                //   will see our resultset update in real time.
+                //  -CONCUR_READ_ONLY means that we cannot accidentally change the
+                //   data in our database by using the .update____() methods of
+                //   the ResultSet class - TableView controls are impacted by
+                //   this setting as well.
+                commStmt = dbConn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                // We send the query to the DB. A ResultSet object is instantiated
+                //  and we are returned a reference to it, that we point to from
+                // dbResults.
+                // Because we declared dbResults at the datafield level
+                // we can see the results from anywhere in our Class.
+                dbResults = commStmt.executeQuery(sqlQuery); // Sends the Query to the DB
+                // The results are stored in a ResultSet structure object
+                // pointed to by the reference variable dbResults
+                // Because we declared this variable globally above, we can use
+                // the results in any method in the class.
+            }
+            catch (SQLException e)
+            {
+                System.out.println(e.toString());
+            }
+        }
 }
